57ff1cf62efa7062d4dd4ac4ab8c2b5ffd601c05
Nicholas Mychka <nickmychka@gmail.com>
2020-09-26 21:19:45 +0300

    add CRUD for shelf

--- server/src/controllers/shelf.js
+++ server/src/controllers/shelf.js
@@ -1,49 +1,58 @@
-/* eslint-disable no-unused-vars */
 const Shelf = require('../models/shelf');
+
+exports.createShelf = (req, res, next) => {
+  Shelf.create(req.body)
+    .then(shelf => {
+      res.json({ data: shelf });
+    })
+    .catch(err => {
+      console.log('exports.createShelf', err);
+    });
+};
+
+exports.updateShelf = (req, res, next) => {
+  const { body, params: { id } } = req;
+
+  Shelf.update(body, { where: { id } })
+    .then(() => Shelf.findByPk(id))
+    .then(shelf => {
+      res.json({ data: shelf });
+    })
+    .catch(err => {
+      console.log('exports.updateShelf', err);
+    });
+};
+
+exports.deleteShelf = (req, res, next) => {
+  const { params: { id } } = req;
+
+  Shelf.destroy({ where: { id } })
+    .then(() => {
+      res.json({ data: { id } });
+    })
+    .catch(err => {
+      console.log('exports.deleteShelf', err);
+    });
+};
 
 exports.getShelves = (req, res, next) => {
   Shelf.findAll()
     .then(shelves => {
       res.json({ data: shelves });
     })
-    .catch(err => console.log('exports.getShelves', err));
+    .catch(err => {
+      console.log('exports.getShelves', err);
+    });
 };
 
-// exports.getProjectById = (req, res, next) => {
-//   const { id } = req.params;
+exports.getShelfById = (req, res, next) => {
+  const { params: { id } } = req;
 
-//   Project.findByPk(id)
-//     .then(project => {
-//       res.json({ data: project });
-//     })
-//     .catch(err => console.log('exports.getProjectById', err));
-// };
-
-// eslint-disable-next-line no-unused-vars
-exports.createShelf = (req, res, next) => {
-  Shelf.create(req.body)
+  Shelf.findByPk(id)
     .then(shelf => {
       res.json({ data: shelf });
     })
-    .catch(err => console.log('exports.createShelf', err));
+    .catch(err => {
+      console.log('exports.getShelfById', err);
+    });
 };
-
-// exports.updateProject = (req, res, next) => {
-//   const { name } = req.body;
-//   const { id } = req.params;
-
-//   Shelf.update({ name }, {
-//     where: { id }
-//   })
-//     .then(() => Project.findByPk(id))
-//     .then(project => res.json({ data: project }))
-//     .catch(err => console.log('exports.updateProject', err));
-// };
-
-// exports.deleteProject = (req, res, next) => {
-//   const { id } = req.params;
-
-//   Project.destroy({ where: { id } })
-//     .then(() => res.json({ data: { id } }))
-//     .catch(err => console.log('exports.deleteProject', err));
-// };
